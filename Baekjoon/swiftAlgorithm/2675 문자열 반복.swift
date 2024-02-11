2dc56b5eb76e170c688d72dc3da6351feb24058e
tlguszz1010 <tlguszz1010@naver.com>
2024-02-11 19:11:47 +0900

    2675 문자열 반복

---
+++ Baekjoon/swiftAlgorithm/2675 문자열 반복.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  swiftAlgorithm
+//
+//  Created by 박시현 on 2/11/24.
+//
+
+import Foundation
+
+let t = Int(readLine()!)!
+var result: String = ""
+for _ in 0..<t {
+    let input = readLine()!.split(separator: " ").map {String($0)}
+    let r = Int(input[0])!, s = Array(input[1])
+    
+    for ele in s {
+        result += String(repeating: ele, count: r)
+    }
+    print(result)
+    result = ""
+}
